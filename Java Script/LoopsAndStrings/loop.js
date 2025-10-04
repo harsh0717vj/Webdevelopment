917fb65be59d62fb80a0ada2c96e9e79877baebf
Harsh Vardhan <hcvardhan0917@gmail.com>
2025-10-04 12:50:53 +0530

    loop.js

---
+++ Java Script/LoopsAndStrings/loop.js
@@ -0,0 +1,30 @@
+//for-loop
+for(let i=1;i<=10;i++){
+    console.log('harsh');
+}
+for(let i=1;i<=5;i++){
+    console.log(i);
+}
+for(let i=5;i>=0;i--){
+    console.log(i);
+}
+for(let i=5;i>=0;i--){
+    if(i==2) break;
+    else console.log(i);
+}
+for(let i=5;i>=0;i--){
+    if(i==2) continue;
+    else console.log(i);
+}
+//while-loop
+let i=1;
+while(i<=10){
+    console.log(i);
+    i++;
+}
+//do-while loop
+let j=1;
+do{
+    console.log(j);
+    j++;
+}while(j<=10);
